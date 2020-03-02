2325aea7404a1a72d3351b30ec3e7cef3ed430ef
Danielo814 <ojedadaniel92@gmail.com>
2020-03-01 21:57:46 -0800

    add build dir

---
+++ build/precache-manifest.1b89f75823e5339808e7e0708a3c389a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e95f0a2174b8fba1a6a74e6c24bb435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99ae042e04be3a430900",
+    "url": "/static/css/main.c5c5e061.chunk.css"
+  },
+  {
+    "revision": "002571eb848d4e399c4c",
+    "url": "/static/js/2.59006d03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.59006d03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99ae042e04be3a430900",
+    "url": "/static/js/main.535752f9.chunk.js"
+  },
+  {
+    "revision": "7124809472c4471ce551",
+    "url": "/static/js/runtime-main.40f74355.js"
+  }
+]);
